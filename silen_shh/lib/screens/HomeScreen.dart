695b3ab8e3bc1da56cd18665e12c481ad66ca6cd
Pranav Singhal <iit2019050@iiita.ac.in>
2020-10-01 14:51:36 +0530

    Added basic sound mode changing functionality  Added Basic Functionality

--- silen_shh/lib/screens/HomeScreen.dart
+++ silen_shh/lib/screens/HomeScreen.dart
@@ -1,11 +1,131 @@
+// This code shows basic implementation of sound modes
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sound_mode/permission_handler.dart';
+import 'package:sound_mode/sound_mode.dart';
+import 'package:sound_mode/utils/sound_profiles.dart';
 
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<HomeScreen> {
+  String _soundMode = 'Unknown';
+  String _permissionStatus;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentSoundMode();
+    getPermissionStatus();
+  }
+
+  Future<void> getCurrentSoundMode() async {
+    String ringerStatus;
+
+    if (!mounted) return;
+
+    setState(() {
+      _soundMode = ringerStatus;
+    });
+  }
+
+  Future<void> getPermissionStatus() async {
+    bool permissionStatus = false;
+    try {
+      permissionStatus = await PermissionHandler.permissionsGranted;
+      print(permissionStatus);
+    } catch (err) {
+      print(err);
+    }
+
+    setState(() {
+      _permissionStatus =
+      permissionStatus ? "Permissions Enabled" : "Permissions not granted";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Silent-Shh'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(child: Text('Give do not disturb access to app in Open Do not Disturb access settings',textAlign: TextAlign.center,)),
+              RaisedButton(
+                onPressed: () => setNormalMode(),
+                child: Text('Set Normal mode'),
+              ),
+              RaisedButton(
+                onPressed: () => setSilentMode(),
+                child: Text('Set Silent mode'),
+              ),
+              RaisedButton(
+                onPressed: () => setVibrateMode(),
+                child: Text('Set Vibrate mode'),
+              ),
+              RaisedButton(
+                onPressed: () => openDoNotDisturbSettings(),
+                child: Text('Open Do Not Access Settings'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  Future<void> setSilentMode() async {
+    String message;
+
+    try {
+      message = await SoundMode.setSoundMode(Profiles.SILENT);
+
+      setState(() {
+        _soundMode = message;
+      });
+    } on PlatformException {
+      print('Do Not Disturb access permissions required!');
+    }
+  }
+
+  Future<void> setNormalMode() async {
+    String message;
+
+    try {
+      message = await SoundMode.setSoundMode(Profiles.NORMAL);
+      setState(() {
+        _soundMode = message;
+      });
+    } on PlatformException {
+      print('Do Not Disturb access permissions required!');
+    }
+  }
+
+  Future<void> setVibrateMode() async {
+    String message;
+
+    try {
+      message = await SoundMode.setSoundMode(Profiles.VIBRATE);
+
+      setState(() {
+        _soundMode = message;
+      });
+    } on PlatformException {
+      print('Do Not Disturb access permissions required!');
+    }
+  }
+
+  Future<void> openDoNotDisturbSettings() async {
+    await PermissionHandler.openDoNotDisturbSetting();
   }
 }
